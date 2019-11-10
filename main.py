4c7a2d0ae9c1d15086ff183b4f6439e3f50ac9a4
Bekieszczuk Karol <karol.bekieszczuk@gmail.com>
2019-11-10 10:18:13 +0000

    fixed errors

--- main.py
+++ main.py
@@ -1,66 +1,66 @@
 import sys
 
 jest_tribbonacci = {}
-bledne_dane = False
+
 
 def pobranie_liczby_przypadkow():
     while True:
         try:
             liczba_przypadkow = int(input())
         except ValueError:
-            print("Liczba przypadkow musi być typu int")
+            print('BLAD')
+            sys.exit(0)
         else:
             if liczba_przypadkow < 0:
-                print("Blad")
+                print('BLAD')
                 sys.exit(0)
             else:
                 return liczba_przypadkow
 
 
 def pobranie_liczby():
-    while True:
-        try:
-            liczba_temp = int(input())
-        except ValueError:
-            print("Podana Liczba musi być typu int")
-        else:
-            return liczba_temp
-
-
-for i in range(0, pobranie_liczby_przypadkow()):
-    liczba_uzytkownika = pobranie_liczby()
-    if liczba_uzytkownika <= 2:
-        bledne_dane = True
+    input_uzytkownika = input()
+    try:
+        liczba_temp = int(input_uzytkownika)
+    except ValueError:
+        jest_tribbonacci[input_uzytkownika] = None
     else:
-        jest_tribbonacci[liczba_uzytkownika] = False
+        if type(liczba_temp) == int:
+            if liczba_temp <= 2:
+                jest_tribbonacci[input_uzytkownika] = None
+            else:
+                return liczba_temp
+
 
 T_minus_3 = 0
 T_minus_2 = 1
 T_minus_1 = 2
-for przypadek in sorted(jest_tribbonacci):
-    while True:
-        wynik = T_minus_1 + T_minus_2 + T_minus_3
+for i in range(0, pobranie_liczby_przypadkow()):
+    przypadek = pobranie_liczby()
+    if type(przypadek) == int:
+        while True:
+            wynik = T_minus_1 + T_minus_2 + T_minus_3
 
-        if wynik == przypadek:
-            jest_tribbonacci[przypadek] = True
-            break
-        else:
-            jest_tribbonacci[przypadek] = False
+            if wynik == przypadek:
+                jest_tribbonacci[przypadek] = True
+                break
+            else:
+                jest_tribbonacci[przypadek] = False
 
-        if wynik > przypadek:
-            break
+            if wynik > przypadek:
+                break
 
-        T_minus_3 = T_minus_2
-        T_minus_2 = T_minus_1
-        T_minus_1 = wynik
-
+            T_minus_3 = T_minus_2
+            T_minus_2 = T_minus_1
+            T_minus_1 = wynik
+    # else:
+    #     jest_tribbonacci[przypadek] = None
 
 for przypadek in jest_tribbonacci:
-    if jest_tribbonacci[przypadek]:
-        print(str(przypadek) + "\tTAK")
+    if jest_tribbonacci[przypadek] is not None:
+        if jest_tribbonacci[przypadek]:
+            print(str(przypadek) + "\tTAK")
+        else:
+            print(str(przypadek) + "\tNIE")
     else:
-        print(str(przypadek) + "\tNIE")
-
-
-if bledne_dane:
-    print("Blad")
+        print('BLAD')
