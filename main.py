628e18f1d4b9ef7e7a11fcce1f1fcf9acca11007
karol <karol121121@gmail.com>
2019-11-13 03:00:48 +0100

    commit after pull

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
-jest_tribbonacci = {}
-exception_counter = 0
+jest_tribbonacci = []
+
 
 def pobranie_liczby_przypadkow():
     while True:
@@ -19,18 +19,15 @@ def pobranie_liczby_przypadkow():
 
 
 def pobranie_liczby():
-    global exception_counter
     input_uzytkownika = input()
     try:
         liczba_temp = int(input_uzytkownika)
     except ValueError:
-        jest_tribbonacci[input_uzytkownika+str(exception_counter)] = None
-        exception_counter += 1
+        jest_tribbonacci.append([input_uzytkownika, None])
     else:
         if type(liczba_temp) == int:
             if liczba_temp <= 2:
-                jest_tribbonacci[input_uzytkownika+str(exception_counter)] = None
-                exception_counter += 1
+                jest_tribbonacci.append([input_uzytkownika, None])
             else:
                 return liczba_temp
 
@@ -39,29 +36,29 @@ T_minus_3 = 0
 T_minus_2 = 1
 T_minus_1 = 2
 for i in range(0, pobranie_liczby_przypadkow()):
+
     przypadek = pobranie_liczby()
     if type(przypadek) == int:
         while True:
             wynik = T_minus_1 + T_minus_2 + T_minus_3
 
             if wynik == przypadek:
-                jest_tribbonacci[przypadek] = True
+                jest_tribbonacci.append([przypadek, True])
                 break
-            else:
-                jest_tribbonacci[przypadek] = False
 
             if wynik > przypadek:
+                jest_tribbonacci.append([przypadek, False])
                 break
 
             T_minus_3 = T_minus_2
             T_minus_2 = T_minus_1
             T_minus_1 = wynik
 
-for przypadek in jest_tribbonacci:
-    if jest_tribbonacci[przypadek] is not None:
-        if jest_tribbonacci[przypadek]:
-            print(str(przypadek) + "\tTAK")
+for i in range(0, len(jest_tribbonacci)):
+    if jest_tribbonacci[i][1] is not None:
+        if jest_tribbonacci[i][1]:
+            print(str(jest_tribbonacci[i][0]) + "\tTAK")
         else:
-            print(str(przypadek) + "\tNIE")
+            print(str(jest_tribbonacci[i][0]) + "\tNIE")
     else:
         print('BLAD')
